b17690d298c99ba9bc7c18f4cf15fb9ede47b336
minji <minjji410@gmail.com>
2015-03-25 23:07:33 +0900

    자바
    file path 는 각자껄로 지정하고 해야 오류가 안납니다

--- JAVA/minji/postIt/PostItGUI.java
+++ JAVA/minji/postIt/PostItGUI.java
@@ -1,17 +1,130 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-
-public class PostItGUI extends JFrame{
-
-	JTextArea ta_content = new JTextArea();
+public class PostItGUI extends JFrame implements ActionListener{
+	PostItManager post_mng = new PostItManager();
+	JFrame mainfrm = new JFrame("PostIt"); //메인 프레임
+	JButton btnLoad = new JButton("Load");
+	JButton btnWrite = new JButton("Write");
+	JButton btnDelete = new JButton("Delete");
+	JButton btnExit = new JButton("EXIT");
+	JButton enter = new JButton("Enter");
+	//JTextArea ta_content = new JTextArea();
+	public void mngGUI(){
+		mainfrm.setBounds(120,120,400,100);
+		mainfrm.setLayout(new BorderLayout());	
+		
+		btnLoad.setBackground(Color.red);
+		btnWrite.setBackground(Color.yellow);
+		btnDelete.setBackground(Color.green);
+		btnExit.setBackground(Color.blue);
+		
+		JPanel menuPanel = new JPanel();	//메뉴 판넬
+		menuPanel.setLayout(new FlowLayout());
+		menuPanel.add(btnLoad);
+		menuPanel.add(btnWrite);
+		menuPanel.add(btnDelete);
+		menuPanel.add(btnExit);
+		
+		btnLoad.addActionListener(this);
+		btnWrite.addActionListener(this);
+		btnDelete.addActionListener(this);
+		btnExit.addActionListener(this);
+		
+		 
+		 mainfrm.add(menuPanel,BorderLayout.NORTH);
+		 mainfrm.setVisible(true);
+	}
 	
-	public PostItGUI(String title, String content) {
-		// TODO Auto-generated constructor stub
-		ta_content.append(title + "\n\n");
-		ta_content.append(content);
-		
-		//변경됬을떄 리스너  -> 저장하는걸
-		
+	public void inputTextInfo() {
+		JFrame subfrm = new JFrame("Text info"); 
+		subfrm.setBounds(120,120,400,100);
+		subfrm.setLayout(new BorderLayout());
+		JPanel input_text = new JPanel();
+		JTextField textTitle = new JTextField();
+		JTextArea textContents = new JTextArea();
+		input_text.setLayout(new GridLayout());
+		input_text.add(new JLabel("  Title"));
+		input_text.add(textTitle);
+		input_text.add(new JLabel("  Contents"));
+		input_text.add(textContents);
+		input_text.add(enter);
+		enter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getSource().equals(enter)){
+					String title = textTitle.getText();
+					String content = textContents.getText();
+					try {
+						post_mng.storePostit(title, content);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					//setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+					subfrm.dispose();
+				}
+			}
+		});
+		subfrm.add(input_text,BorderLayout.CENTER);
+		subfrm.setVisible(true);
+	}
+	public void inputDeleteInfo(){
+		JFrame subfrm = new JFrame("Delete Info"); //메인 프레임
+		subfrm.setBounds(120,120,400,100);
+		subfrm.setLayout(new BorderLayout());
+		JPanel input_text = new JPanel();
+		JTextField textTitle = new JTextField();
+		input_text.setLayout(new GridLayout());
+		input_text.add(new JLabel("  Title"));
+		input_text.add(textTitle);
+		input_text.add(enter);
+		enter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getSource().equals(enter)){
+					String title = textTitle.getText();
+					post_mng.deletePostit(title);
+					subfrm.dispose();
+				}
+			}
+		});
+		subfrm.add(input_text,BorderLayout.CENTER);
+		subfrm.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(btnLoad)){
+				try {
+					post_mng.loadFile();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		}
+		else if(e.getSource().equals(btnWrite)){
+				inputTextInfo();
+		}
+		else if(e.getSource().equals(btnDelete)){
+				inputDeleteInfo();
+		} 
+		else
+			System.exit(0);
 	}
 }
--- JAVA/minji/postIt/PostItManager.java
+++ JAVA/minji/postIt/PostItManager.java
@@ -1,21 +1,24 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.TextArea;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class PostItManager {
+	private String file_path = "C:/Users/HMJ/Documents/postIt/";
 	ArrayList<File> txtFile = new ArrayList<File>();
-	private String default_path = "postfiles";
 		//각파일에 대해서 내용은 GUI 로 만들어서 뿌려준다.
 			//1. 파일을 읽어들인다.
 			//2. 각각의 파일들을 (내용읽기) GUI로 만든다.
 	public void loadFile() throws IOException{		//파일에 내용 불러오기
-		String file_path = "C:/Users/HMJ/Documents/postIt/";
-		
+		txtFile.removeAll(txtFile);
+		String file_loadpath;
 		File f = new File(file_path);
 		if(!f.exists()){
 			System.out.println("파일이 없습니다");
@@ -27,25 +30,45 @@ public class PostItManager {
 				txtFile.add(file);
 			}
 		}
+		JFrame subfrm = new JFrame("Text info"); 
+		subfrm.setBounds(120,120,400,400);
+		subfrm.setLayout(new BorderLayout());
+		JPanel postItPanel = new JPanel();	//포스트잍 보이는 판넬
+		postItPanel.setLayout(new GridLayout());
+		TextArea loadText = new TextArea();
 		for(File file : txtFile){
-			file_path = "C:/Users/HMJ/Documents/postIt/";	//초기화
+			file_loadpath = file_path;	//초기화
 			System.out.println("파일 이름 : "+file.getName());
-			file_path += file.getName();
-			File inFile = new File(file_path);
+			file_loadpath += file.getName();
+			File inFile = new File(file_loadpath);
 			BufferedReader br = new BufferedReader(new FileReader(inFile));
-			String line = null;
-				
+			String line = null;	
 			while((line = br.readLine())!= null){
+				loadText.append(line+"\n");
 				System.out.println(line);
 			}
 			br.close();
-			}	
+		}
+		//loadText.append(str+"\n");
+		postItPanel.add(loadText);
+		subfrm.add(postItPanel,BorderLayout.CENTER);
+		subfrm.setVisible(true);
 	}
 	
-	public void storePostit(String title, String content) throws IOException{
-		///
-		//파일에쓴다
-		String makeFile = "C:/Users/HMJ/Documents/postIt/";
+	public void storePostit(String title, String content) throws IOException{		//파일에쓴다
+		/*		파일 이름 안겹치게 생성하려고 만들었다가 지운 부분
+		for(File file : txtFile){
+			if(file.getName() == title){
+				System.out.println("파일 이름이 겹칩니다.");
+				while(file.getName() == title){
+					title+="num";
+					num++;
+				}
+				break;
+			}
+		}
+		*/
+		String makeFile = file_path;
 		makeFile += title+".txt";
 		File file = new File(makeFile);
 		FileWriter  fw = new FileWriter(file, true);
@@ -53,14 +76,13 @@ public class PostItManager {
 		fw.flush();
 		fw.close();
 	}
-	public void deletePostit(String title){
-			File file = new File("C:/Users/HMJ/Documents/postIt/"+title+".txt");
+	public void deletePostit(String title){					//삭제하기
+			File file = new File(file_path+title+".txt");
 			if(file.exists()){
 				file.delete();
 			}
 			else{
 				System.out.println("파일이 없습니다.");
 			}
-			
 	}
 }
